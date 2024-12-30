1f9a0d5fbc0a29c7b925324246a9601f75d957ab
Sanchir01 <emgushovs@mail.ru>
2024-12-31 01:03:48 +0300

    add base counter

--- programs/solana/src/lib.rs
+++ programs/solana/src/lib.rs
@@ -7,12 +7,16 @@ pub mod solana {
     use super::*;
 
     pub fn initialize(ctx: Context<Initialize>) -> Result<()> {
+        let counter = &mut ctx.accounts.counter;
+        counter.bump = ctx.bumps.counter;
         msg!("Greetings from: {:?}", ctx.program_id);
         Ok(())
     }
-    pub fn increment(ctx: Context<Icrement>) -> Result<()>{
-        let counter = &mut  ctx.accounts.counter;
-        msg!("Counter incremented! Current count :{}", counter.count)
+    pub fn increment(ctx: Context<Increment>) -> Result<()> {
+        let counter = &mut ctx.accounts.counter;
+        msg!("Counter incremented! Current count :{}", counter.count);
+        counter.count = counter.count.checked_add(1).unwrap();
+        msg!("Counter incremenred! Current count: {}", counter.count);
         Ok(())
     }
 }
@@ -24,20 +28,27 @@ pub struct Initialize<'info> {
 
     #[account(
         init,
+        seeds = [b"solana"],
+        bump,
         payer = user,
-        space = 8 + 8
+        space = 8 + Counter::INIT_SPACE
     )]
-    pub counter: Accounts<'info,Counter>,
-    pub system_proram:Program<'info,System>
+    pub counter: Account<'info, Counter>,
+    pub system_program: Program<'info, System>,
 }
 
 #[derive(Accounts)]
-struct Increment<'info> {
-    #[account(mut)]
-    pub counter: Accounts<'info,Counter>
+pub struct Increment<'info> {
+    #[account(mut,
+        seeds = [b"solana"],
+        bump = counter.bump
+    )]
+    pub counter: Account<'info, Counter>,
 }
 
 #[account]
-pub struct Counter{
-    pub count: u64
+#[derive(InitSpace)]
+pub struct Counter {
+    pub count: u64,
+    pub bump: u8,
 }
--- tests/solana.ts
+++ tests/solana.ts
@@ -1,16 +1,33 @@
 import * as anchor from "@coral-xyz/anchor";
 import { Program } from "@coral-xyz/anchor";
 import { Solana } from "../target/types/solana";
-
+import { PublicKey } from "@solana/web3.js";
 describe("solana", () => {
-  // Configure the client to use the local cluster.
-  anchor.setProvider(anchor.AnchorProvider.env());
+  const provilder = anchor.AnchorProvider.env();
+  anchor.setProvider(provilder);
 
   const program = anchor.workspace.Solana as Program<Solana>;
+  const [solanaPDA] = PublicKey.findProgramAddressSync(
+    [Buffer.from("solana")],
+    program.programId
+  );
 
   it("Is initialized!", async () => {
-    // Add your test here.
-    const tx = await program.methods.initialize().rpc();
-    console.log("Your transaction signature", tx);
+    try {
+      const txSign = await program.methods.initialize().rpc();
+      const accountData = await program.account.counter.fetch(solanaPDA);
+
+      console.log(`trancsactionSignature: ${txSign}`);
+      console.log(`Count : ${accountData.count}`);
+    } catch (e) {
+      console.log(`initialize error: ${e}`);
+    }
+  });
+  it("Increment", async () => {
+    const trancsactionSignature = await program.methods.increment().rpc();
+
+    const accountData = await program.account.counter.fetch(solanaPDA);
+    console.log(`trancsactionSignature: ${trancsactionSignature}`);
+    console.log(`Count :${accountData.count}`);
   });
 });
